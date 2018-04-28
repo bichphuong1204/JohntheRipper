0f0c11752153a2e0836ac3af97b52c92729fc1e8
bichphuong1204 <36155692+bichphuong1204@users.noreply.github.com>
2018-04-29 02:31:48 +0300

    Update Build_Mkpasswd.sh

--- Build_Mkpasswd.sh
+++ Build_Mkpasswd.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#install mkpasswd
-sudo apt-get install whois
-
 #create password file
 cd $HOME/Ripper/JohnTheRipper/run
 touch passfile.txt
@@ -16,15 +13,6 @@ echo -n ${passwd}: ; mkpasswd -m des ${passwd} >>passfile.txt
 echo -n ${passwd}: ; mkpasswd -m sha-256 ${passwd} >>passfile.txt
 echo -n ${passwd}: ; mkpasswd -m sha-512 ${passwd} >>passfile.txt
 
-#create a running script file
-touch running.sh
-echo "" > running.sh
+#Running
+mpirun -np ${N} ./john passfile.txt
 
-#Running with different re-encrypt format
-echo 'mpirun -np ${N} ./john --format=aix-smd5 --show passfile.txt' >>running.sh
-echo 'mpirun -np ${N} ./john --format=descrypt --show passfile.txt' >>running.sh
-echo 'mpirun -np ${N} ./john --format=sha256crypt --show passfile.txt' >>running.sh
-echo 'mpirun -np ${N} ./john --format=sha512crypt --show passfile.txt' >>running.sh
-
-chmod +x running.sh
-./running.sh
